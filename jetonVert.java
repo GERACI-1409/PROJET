6bb474177c62cc7a903e0604d02995baba004990
GERACI-1409 <58684765+GERACI-1409@users.noreply.github.com>
2019-12-09 10:02:16 +0100

    Create jetonVert.java

---
+++ jetonVert.java
@@ -0,0 +1,13 @@
+
+public class jetonVert extends Jeton{
+
+  private final int valeur = 100 ;
+
+  public jetonVert(int valeur, String couleur){
+    super(valeur,"Vert") ;
+  }
+
+  public int getValeurVert(){
+    return valeur ;
+  }
+}
