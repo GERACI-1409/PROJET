09c5c4a594912c990b88d5684b879040481dadd9
trsjulie <37087267+trsjulie@users.noreply.github.com>
2019-12-14 14:15:42 +0100

    Add files via upload

---
+++ gestion/Menu.java
@@ -0,0 +1,17 @@
+package gestion;
+
+import java.util.ArrayList;
+
+public class Menu{
+    
+    Hotel h;
+    ArrayList<String> actions;
+    
+    pubic Menu(Hotel h){
+	this.h = h;
+	actions = new ArrayList<String>();
+    }
+
+    
+
+}
