86154f4ae8a1c061f7d1571181fdfe7da96ea623
julie <julie.tores@gmail.com>
2019-12-20 12:37:47 +0100

    comentaire ok

--- spa/Spa.java
+++ spa/Spa.java
@@ -3,20 +3,39 @@ package spa;
 
 import personne.*;
 public class Spa{
+    /* *nom du spa*/
     private String nom;
 
+    ///////// constructeur ///////////////
     public Spa(String nom){
 	this.nom = nom;
     }
 
+
+    public Spa(){
+	this("Yuma Massage");
+    }
+	
+    //////////// geters /////////////////
+    /**
+     * Methode qui retourne le nom du spa
+     */
     public String getNom(){
 	return nom;
     }
 
+    /////////////// seters ///////////////
+    /**
+     * Modifie le nom du spa
+     */
     public void setNom(String s){
 	nom = s;
     }
+    ///////////////////////// autres méthodes //////////////////////////
 
+    /**
+     * Méthode qui affiche la description et les prix des massages du spa Yuma Massage
+     */
     public static void affichePrix(){
 	String l0 = "**************** Bienvenue au Spa Yuma Massage ********************";
 	String l1 = "MASSAGE RELAXANT: A la fois relaxant et profond, il vous plongera dans une totale détente musculaire, physique et psychique. \nPRIX: 50 euros";
@@ -26,6 +45,10 @@ public class Spa{
 
     }
 
+
+    /**
+     * Methode qui affiche les massages disponibles.
+     */
     public static void afficheMenuSpa(){
 	System.out.println("Quel massage souhaitez vous");
 	System.out.println("1: Massage relaxant");
@@ -33,12 +56,15 @@ public class Spa{
 	System.out.println("3: Massage a l'elixir de bougie");
     }
 
+    /**
+     * Affiche le prix du massage en fonction du massage choisi
+     */
     public static void dernierAffichage(int i, Client c){
 	int prix;
 	System.out.println("Rendez vous dans la salle de massage");
 	if(i == 1) prix = 50;
 	if(i == 2) prix = 70;
 	if(i == 3) prix = 80;
-	System.out.println(""+i+" euros sont ajoutes sur votre note.");
+	System.out.println(""+prix+" euros sont ajoutes sur votre note.");
     }
 }
