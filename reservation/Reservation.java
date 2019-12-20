9e6787310c0698d2aa6cc5dad37b30ddf8eff6d3
julie <julie.tores@gmail.com>
2019-12-20 12:31:14 +0100

    comentaire ok

--- reservation/Reservation.java
+++ reservation/Reservation.java
@@ -84,6 +84,10 @@ public class Reservation{
     }
 
 
+    /** *Modifie la liste des clients de la chambre*/
+    public void setCliets(Client[] c){
+	clients = c;
+    }
 
 
 
@@ -92,13 +96,22 @@ public class Reservation{
 
 
     
-
+    /**
+     * Methode qui permet d'ajouter un client d'ajouter
+     * un client si le tableau n'est pas deja complet
+     * Retourne true si on a reussi, false sinon
+     */
     public boolean ajouterClient(Client c){
 	int n = clients.length;
 	int i = 0;
+	// tant qu'on ne depasse pas la taille du taleau
+	// et que la case courante n'est pas nulle
+	// on ajoute un à l'indice
 	while(i < n && clients[i]!=null){
 	    i++;
 	}
+	// si on n'a pas atteint la fin du tableau on
+	// peut ajouter le client
 	if(i<n){
 	    clients[i] = c;
 	    attribueTicket();
@@ -107,45 +120,66 @@ public class Reservation{
 	return false;
     }
 
+    /**
+     * Retourne une chaine de caractere contenant la date de début,
+     * la date de fin, et la liste des clients.
+     */
     public String toString(){
 	String s = "";
 	for(int i = 0; i < clients.length; i++){
 	    if (clients[i] != null){
 		s += clients[i].toString() + "\n";
-	    }
-		
+	    }	
 	}
-	    
-	return "du " +debut.toString()  + " au " + fin.toString() +s ;
+	return "du " +debut.toString()  + " au " + fin.toString() +s;
     }
 
+    /**
+     * Permet de calculer la duree de la reservation.
+     */
     public void calculeDuree(){
         Duration duration = Duration.between(debut,fin);
 	duree =(int) Math.abs(duration.toDays());
 	
     }
 
+    /**
+     * Methode qui calcule le nombre de ticket que le client va recevoir
+     * en fonction de la duree de son sejour.
+     */
     public void attribueTicket(){
-	int n = duree * 2;
+	int n = duree * 2; // n est le nombre de ticket que le client va recevoir
+	//Parcours du tableau de client
 	for(int i = 0; i <clients.length ;i++){
+	    // si la case n'est pas nulle 
 	    if(clients[i] != null){
+		// on met à jour le nombre de ticket.
 		clients[i].setNbTicketRestau(n);
 		clients[i].setSortis(n);
-		    }
+	    }
 	}
     }
 
+    /**
+     * Methode qui permet de trouver un client dans 
+     * le tableau de clients à partir de son mail.
+     */
     public Client trouveClient(String mail){
 	int n = clients.length;
 	int i = 0;
 	boolean trouve = false;
 	Client c = null;
+	// tant qu'on a pas atteint la fin du tableau
+	// et qu'on a pas trouve le client 
 	while(i < n && !trouve){
 	    c = clients[i];
+	    // on verifie si le client courrant à le bon mail
 	    if(c.getEmail().equals(mail)) trouve = true;
 	    i++;
 	}
+	// si on a trouve le client on le renvoie
 	if(trouve) return c;
+	// sinon on retourne null
 	else return null;
     }
 
